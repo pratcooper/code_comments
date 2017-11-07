e8741e20495e3f38179c3201c4239ef12abcfbb1
pratcooper <31424351+pratcooper@users.noreply.github.com>
2017-11-06 22:31:38 -0800

    Add files via upload
    Updated file mergedCosineSimCode.py

--- mergedCosineSimCode.py
+++ mergedCosineSimCode.py
@@ -2,6 +2,9 @@ import nltk, string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import wordnet as wn
 import re
+import csv
+import gensim
+import numpy as np
 
 # nltk.download('wordnet')
 # nltk.download('punkt') # if necessary...
@@ -44,18 +47,24 @@ def normalize(text):
             for i in res:
                 result_list_tokens.append(i)
 
-    print("Final result passed to TfidfVectorizer:",result_list_tokens)
+    #print("Final result passed to TfidfVectorizer:",result_list_tokens)
     return result_list_tokens
 
+def raw_normalize(text):
+    return stem_tokens(nltk.word_tokenize(text.translate(remove_punctuation_map)))
 
 vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english',lowercase = False)
-
+raw_vectorizer = TfidfVectorizer()
 
 def cosine_sim(text1, text2):
     tfidf = vectorizer.fit_transform([text1, text2])
     # print(tfidf)
     return ((tfidf * tfidf.T).A)[0, 1]
 
+def raw_cosine_sim(text1, text2):
+    tfidf = raw_vectorizer.fit_transform([text1, text2])
+    # print(tfidf)
+    return ((tfidf * tfidf.T).A)[0, 1]
 
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -93,7 +102,37 @@ def replaceSynonyms(text1, text2):
 
     return text1, text2
 
+def getPair(line):
 
+    counter = 0
+    codeList = []
+    commentList = []
+
+    while(counter<len(line)):
+        methodInfo = line[counter+0]
+        pathToMain = line[counter+1]
+        #print(line[1])
+        commentLength = int(line[counter+2])
+
+        comment = []
+        for i in range(commentLength):
+            comment.append(line[counter + i + 3])
+        commentString = ' '.join(comment)
+        commentList.append(commentString)
+        codeLength = int(line[counter+ 3 + commentLength])
+
+        code = []
+        for i in range(codeLength):
+            code.append(line[counter + i+4+commentLength])
+        codeString = ' '.join(code)
+        codeList.append(codeString)
+        end = counter + commentLength+codeLength+5
+        counter = end
+        #print(counter)
+    return codeList,commentList
+
+
+'''
 print("Original text")
 print(document1)
 print(document2)
@@ -107,7 +146,7 @@ print(text1)
 print(text2)
 
 print(cosine_sim(text1, text2))
-
+'''
 
 # print(cosine_sim(document1, document4))
 # print(cosine_sim(document1, document5))
@@ -149,10 +188,108 @@ print(cosine_sim(text3, text4))
 '''
 #print(cosine_sim(document5, document6))
 
+def runCosineSim(document1,document2):
+    #print("Original text")
+    #print(document1)
+    #print(document2)
+    cosine_before = cosine_sim(document1, document2)
+    #print("cosine_before :" , cosine_before )
+    text1, text2 = replaceSynonyms(document1, document2)
+    #print('replaced text :', text2)
+    text1 = ' '.join(text1)
+    text2 = ' '.join(text2)
+    #print("Preprocessed text")
+    #print(text1)
+    #print(text2)
+    cosine_after  = cosine_sim(text1, text2)
+    #print("cosine_after :", cosine_after)
+    if cosine_before != cosine_after:
+        print("Values changed")
+
+    return cosine_after
+
+def runRawCosineSim(document1,document2):
+    raw_res_cosine = raw_cosine_sim(document1, document2)
+    if raw_res_cosine == 0:
+        print(document1)
+        print(document2)
+
+    return raw_res_cosine
+
+def runCosineSimWord2Vec(document1,document2,model1,model2):
+
+    result_vec_code = np.zeros(shape=(100,))
+    for word in document1:
+        try:
+            word_vec = model1[word]
+        except KeyError:
+            word_vec = 0
+        result_vec_code = np.add(result_vec_code,word_vec)
+
+    result_vec_code = np.divide(result_vec_code,len(document1))
+
+    result_vec_comment = np.zeros(shape=(100,))
+    for word in document2:
+        try:
+            word_vec = model2[word]
+        except KeyError:
+            word_vec = 0
+        result_vec_comment = np.add(result_vec_comment, word_vec)
+
+        result_vec_comment = np.divide(result_vec_comment, len(document2))
+
+    numerator = np.dot(result_vec_code,result_vec_comment)
+    dinominator = np.multiply(np.linalg.norm(result_vec_code),np.linalg.norm(result_vec_comment))
+
+    cosine_similar= float(numerator/dinominator)
+
+    return cosine_similar
+
+if __name__=="__main__":
+    resCosineValList = []
+    resRawCosineValList = []
+    with open("/Users/prathameshnaik/PycharmProjects/DR_Code/tp") as f:
+        lines = f.readlines()
+
+        codeList, commentList = getPair(lines)
+        cnt = 1
+
+        #model word2Vec
+        #model = gensim.models.Word2Vec(sentences, min_count=1)
+        sentences = ""
+        for i in codeList:
+            sentences = sentences + i
+
+        model1 = gensim.models.Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
+        sentences = ""
+
+        for i in commentList:
+            sentences = sentences + i
+
+        model2 = gensim.models.Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
+
+
+        for i,j in zip(codeList,commentList):
+            print('dataset :' ,cnt)
+            cnt = cnt + 1
+            res_cosine = runCosineSim(i, j)
+            resCosineValList.append(res_cosine)
+            raw_res_cosine = runRawCosineSim(i,j)
+            resRawCosineValList.append(raw_res_cosine)
+            res_cosine_word2vec = runCosineSimWord2Vec(i,j,model1,model2)
+
+            print("Raw value:",raw_res_cosine)
+            print("Modified value using features:", res_cosine)
+            print("Modified value using word2vec:", res_cosine_word2vec)
+            print("-----------")
+
+    with open("/Users/prathameshnaik/PycharmProjects/DR_Code/data.csv",'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        for i,j in zip(resCosineValList,resRawCosineValList):
+            wr.writerow([float(i),float(j)])
 
 
 
-# print("Preprocessed text")
 # print (text3)
 # print (text4)
 # print(cosine_sim(text3, text4))
